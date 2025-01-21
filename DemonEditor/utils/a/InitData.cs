e4b3e9fe1f4a95b4f2c8c750d372e4d7739e4865
Facundo <sosafacun@proton.me>
2025-01-21 16:45:48 -0300

    it reads, loads and inits the lists from the .json files

--- DemonEditor/utils/a/InitData.cs
+++ DemonEditor/utils/a/InitData.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using Newtonsoft.Json;
 using Godot;
 
 public class InitData{
@@ -11,36 +12,36 @@ public class InitData{
 
     public InitData(){}
 
-    private void LoadJsons(){
+    public void LoadJsons(){
 
         //Read, parse and load resistances from the .json file into a List
-        string resistancesJson = File.ReadAllText("./data/resistances.json");
+        string resistancesJson = File.ReadAllText("./DemonEditor/data/resistances.json");
         var resistancesData = JsonConvert.DeserializeObject<Dictionary<string, List<Resistance>>>(resistancesJson);
         resistances = resistancesData["resistances"];
 
         //Read, parse and load elements from the .json file into a List
-        string elementsJson = File.ReadAllText("./data/elements.json");
+        string elementsJson = File.ReadAllText("./DemonEditor/data/elements.json");
         var elementsData = JsonConvert.DeserializeObject<Dictionary<string, List<Element>>>(elementsJson);
         elements = elementsData["elements"];
 
         //Read, parse and load ailments from the .json file into a List
-        string ailmentsJson = File.ReadAllText("./data/ailments.json");
+        string ailmentsJson = File.ReadAllText("./DemonEditor/data/ailments.json");
         var ailmentsData = JsonConvert.DeserializeObject<Dictionary<string, List<Ailment>>>(ailmentsJson);
         ailments = ailmentsData["ailments"];
         
         //Read, parse and load races from the .json file into a List
-        string racesJson = File.ReadAllText("./data/races.json");
+        string racesJson = File.ReadAllText("./DemonEditor/data/races.json");
         var racesData = JsonConvert.DeserializeObject<Dictionary<string, List<Race>>>(racesJson);
         races = racesData["races"];
 
         //Read, parse and load stats from the .json file into a List
-        string statsJson = File.ReadAllText("./data/stats.json");
+        string statsJson = File.ReadAllText("./DemonEditor/data/stats.json");
         var statsData = JsonConvert.DeserializeObject<Dictionary<string, List<Stat>>>(statsJson);
         stats = statsData["stats"];
     }
 
     public void showResistances(){
-        foreach(element in elements){
+        foreach(Element element in elements){
             GD.Print(element.Name);
         }
     }
