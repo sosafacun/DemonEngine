d57492c7f111bddca279bf53f3c91fecc472191e
Facundo <sosafacun@proton.me>
2025-01-22 23:55:30 -0300

    elements and ailments can now get removed (each " copy.json" is the backup)

--- DemonEditor/scenes/edit_button/EditButton.cs
+++ DemonEditor/scenes/edit_button/EditButton.cs
@@ -21,10 +21,15 @@ public partial class EditButton : Button
 	}
 
 	private void EditButtonPressed(){
+		//checks if it's not the "Edit Button" (so delete button)
 		if(!isEditButton){
+			//checks if its an ailment or an element
 			if(node.isElemental){
+				//makes an object of the selected element to delete
 				Element elementToDelete = InitData.elements.Find(e => e.Name == label.Text);
+				//removes that object from the list
 				InitData.elements.Remove(elementToDelete);
+				//saves that removal to the .json
 				SaveToJson("./DemonEditor/data/affinity/element/elements.json",InitData.elements,"elements");
 			} else {
 				Ailment ailmentToDelete = InitData.ailments.Find(a => a.Name == label.Text);
@@ -34,12 +39,14 @@ public partial class EditButton : Button
 		}
 	}
 	private void SaveToJson<A>(String save_path,List<A> affinties, string jsonName) where A: IAffinity{
-
+		//set the Dictionary key
 		var affinityData = new Dictionary<string, List<A>>{
 			{jsonName, affinties}
 		};
 
+		//stringify the list to parse to .json
 		string updatedAffinityData = JsonConvert.SerializeObject(affinityData, Formatting.Indented);
+		//save that .json
 		using (StreamWriter sw = new StreamWriter(save_path)){sw.Write(updatedAffinityData);}
 	}
 }
